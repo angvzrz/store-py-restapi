ac6a8c58c428719f2da403968e85dfe92991f5f1
Angel Osiel <ang_vzrz@live.com.mx>
2019-05-30 00:22:05 -0500

    The POST of an item now requires a JWT fresh token.

--- resources/item.py
+++ resources/item.py
@@ -1,5 +1,5 @@
 from flask_restful import Resource, reqparse
-from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity
+from flask_jwt_extended import jwt_required, get_jwt_claims, jwt_optional, get_jwt_identity, fresh_jwt_required
 from models.item import ItemModel
 
 class Item(Resource):
@@ -30,6 +30,7 @@ class Item(Resource):
 
 		return {'message':'Item not found'}, 404
 
+	@fresh_jwt_required
 	def post(self, name):
 		if ItemModel.find_by_name(name):
 			return {'message':"An item with the name '{}' already exists".format(name)}, 400
